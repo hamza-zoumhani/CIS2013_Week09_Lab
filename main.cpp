861054c5a7f14961c33d41eb4ece11abe2fc2e75
Hamza Zoumhani <hamza.zoumhani@okstate.edu>
2019-03-13 19:13:51 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,21 @@ using namespace std;
 
 int main(){
 	
+	char chess[16][16];
 	
+	for(int i; i=0; i<16; i++){
+		
+		for(int j; j=0; j<16; j++){
+				chess[i][j] = '-';
+		}
+	}
+	
+	for(int i; i=0; i<16; i++){
+		
+		for(int j; j=0; j<16; j++){
+				chess[i][j] = '-';
+		}
+	}
 	
 	return 0;
 }
